916d0771e7d3f335d635c2b2619546e9dba7eb72
adii1203 <aditya32ft@gmail.com>
2024-10-27 22:50:49 +0530

    Refactor middleware to handle public routes and redirect authenticated users to the dashboard

--- middleware.ts
+++ middleware.ts
@@ -1,10 +1,19 @@
 import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
 
 const isProtectedRouts = createRouteMatcher(["/v(.*)"]);
+const isPublicRouts = createRouteMatcher(["/"]);
 
 export default clerkMiddleware(async (auth, req) => {
   if (isProtectedRouts(req)) {
     await auth.protect();
+  }
+
+  const { userId } = await auth();
+
+  if (userId && isPublicRouts(req)) {
+    // redirect to dashboard
+    const { nextUrl } = req;
+    return Response.redirect(new URL("/v/colloction", nextUrl));
   }
 });
 
